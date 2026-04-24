b5c9a4ecc71148d947f23a2d7d09c8f0ca9c229c
shivaapratim <shuklashivang9@gmail.com>
2026-04-25 04:50:44 +0530

    first commit

--- Math(LEC_9)/BinaryToDec.java
+++ Math(LEC_9)/BinaryToDec.java
@@ -1,11 +1,18 @@
 public class BinaryToDec {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int n = 10010;
 
-        while(n>0){
+        int base = 1;
+        int ans = 0;
 
-            int b
+        while (n > 0) {
+            int b = n % 10;
+            ans += b * base;
 
+            base *= 2;
+            n /= 10;
         }
+
+        System.out.println(ans);
     }
 }
